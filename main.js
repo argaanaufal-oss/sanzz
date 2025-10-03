dc0168ab7c0393015121448709bc7b85fb222318
argaanaufal-oss <argaanaufal@gmail.com>
2025-10-03 18:23:44 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let hari = new Date().getDay()
+alert ("hari ini adalah hari ke " + hari)
+alert ("selamat datang")
+
+btn1.style.border = 'none'
+btn1.style.padding = '1px'
+btn1.style.fontSize = '24px'
+btn1.style.background = 'aqua'
+
